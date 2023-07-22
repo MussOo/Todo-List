59d5b790962eb43caa70e119dcc70d01047b6fac
Salmi Mustapha <mustapha.salmi08000@gmail.com>
2023-07-22 11:01:07 +0200

    set component main

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
 import style from './Home.module.scss';
+import TodoList from './TodoList/TodoList';
 
 function Home() {
-  return <div className={style.container}>Hello world</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.container_main}>
+        <h1>TODOLIST</h1>
+        <TodoList />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
